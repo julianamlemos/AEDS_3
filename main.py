d47b2d3c5510061347da4e4bd922852d2fedf5d5
Juliana M. Lemos <42423350+julianamlemos@users.noreply.github.com>
2022-05-16 21:39:05 -0300

    Update main.py
    adição de comentários, formatação da saída da informação de tempo

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from grafo import Grafo
 import time
 
 grafo = Grafo()
-arquivo = input("Informe o nome do arquivo e sua extensão (Ex: nome.txt): ")
+arquivo = input("\nInforme o nome do arquivo e sua extensão (Ex: nome.txt): ")
 origem = int(input("Informe o vértice de origem: "))
 destino = int(input("Informe o vértice de destino: "))
 start = time.process_time()
-print("Processando...")
+print("\nProcessando...\n")
 
 grafo.caminhoMinimo(arquivo, origem, destino)
 end = time.process_time()
 tempo = end- start
-temp_format = "{:.3f}".format(tempo)
-print("Tempo: " + temp_format + " s")
+temp_format = "{:.3f}".format(tempo) #formatação para o tempo aparecer com 3 casas decimais
+print("Tempo: " + temp_format + "s\n")
 
